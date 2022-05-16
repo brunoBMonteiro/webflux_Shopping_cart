85b1009a7d8e2480700b946fefa737a8a71809a2
brunoBMonteiro <brunobilheri@hotmail.com>
2022-05-16 20:21:25 -0300

    Add anottations in controller for swagger

--- src/main/java/com/shoppingcart/shopping_cart_webflux/product/controller/ProductController.java
+++ src/main/java/com/shoppingcart/shopping_cart_webflux/product/controller/ProductController.java
@@ -2,6 +2,12 @@ package com.shoppingcart.shopping_cart_webflux.product.controller;
 
 import com.shoppingcart.shopping_cart_webflux.product.domain.Product;
 import com.shoppingcart.shopping_cart_webflux.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,33 +22,65 @@ import javax.validation.constraints.NotNull;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("product")
+@Tag(name = "Product", description = "Uma api que simula um shopping cart service")
 public class ProductController {
     private final ProductService productService;
 
+    @Operation(summary = "Obter uma lista de produtos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "encontrou lista", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "204", description = "Tem uma lista mas está vazia", content = @Content),
+            @ApiResponse(responseCode = "404", description = "lista de produtos não encontrada", content = @Content)
+    })
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public Flux<Product> listAll(){
         return productService.findAll();
     }
 
+    @Operation(summary = "Obter um produto por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "encontrou o produto", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado", content = @Content)
+    })
     @GetMapping(path = "{id}")
     @ResponseStatus(HttpStatus.OK)
     public Mono<Product> findById(@PathVariable Long id){
         return productService.findById(id);
     }
 
+    @Operation(summary = "Salva um novo produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "salvou novo produto", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "400", description = "Campo do produto em branco", content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Product> createProduct(@Valid @NotNull @NotEmpty @RequestBody Product product){
         return productService.save(product);
     }
 
+    @Operation(summary = "Atualiza informações de um produto por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Informações do produto atualizadas", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado", content = @Content)
+    })
     @PutMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Mono<Void> updateProduct(@PathVariable Long id, @RequestBody Product product) {
         return productService.update(product.withId(id));
     }
 
+    @Operation(summary = "Deleta um produto por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "produto deletado com sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado", content = @Content)
+    })
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteProduct(@PathVariable Long id){
